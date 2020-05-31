69dc279c37426b4021d04c68f23112584495a609
José Alexandre Ramos Neto <65056403+alexandre-ramos94@users.noreply.github.com>
2020-05-31 19:23:18 -0300

    updates

---
+++ jokenpo.js
@@ -0,0 +1,35 @@
+/*function pedra(){
+    //Esta função vai sortear numeros entre 1 e 3, conectando cada resultado a uma imagem de pedra papel ou tesoura//
+    let pick = Math.floor(Math.random() *(4-1)+(1))
+    alert(pick)
+
+}*/
+function pedra(){
+    let imagem = document.querySelector('#ima')
+    imagem.src='images/paper.png'
+    compara(1)
+
+ 
+    
+}
+
+function papel(){
+    let imagem = document.querySelector('#ima')
+    imagem.src='images/scissor.png'
+    compara(2)
+
+
+}
+function tesoura(){
+    let imagem = document.querySelector('#ima')
+    imagem.src='images/rock.png'
+    compara(3)
+}
+
+function compara(x){
+    let res = document.querySelector('#resposta')
+    if (x==1){res.innerHTML='PAPEL';}
+    else if (x==2){res.innerHTML='TESOURA'}
+    else if(x==3){res.innerHTML='PEDRA'} 
+}
+
